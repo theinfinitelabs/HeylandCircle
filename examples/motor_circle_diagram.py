db572cab22a75457ad2f69711c46a9e09777f7ed
buffanu <anubhav.gupta@colorado.edu>
2026-06-06 14:41:23 -0600

    Slight fomatting of hte comments in example script.

--- examples/motor_circle_diagram.py
+++ examples/motor_circle_diagram.py
@@ -21,20 +21,20 @@ def main():
         I_0=6,               # No-load current [A]
         phi_0_deg=85,        # No-load current angle [deg]
         I_sc=12.0,           # Blocked-rotor current [A]
-        phi_sc_deg=69.0667, # Blocked-rotor current angle [deg]
-        V_rated=400,        # Rated voltage [V]
-        V_sc=100,           # Blocked-rotor test voltage [V]
-        P_rated_kw=5.6      # Rated power [kW]
+        phi_sc_deg=69.0667,  # Blocked-rotor current angle [deg]
+        V_rated=400,         # Rated voltage [V]
+        V_sc=100,            # Blocked-rotor test voltage [V]
+        P_rated_kw=5.6       # Rated power [kW]
     )
 
     # Circle diagram configuration
     config = CircleDiagramConfig(
-        power_scale=1.4,          # x kW/cm
+        power_scale=1.4,           # x kW/cm
         current_scale=1 / 2,       # 1/x implies 1 cm = x Amps
-        show_eff_scale=True,       # w hether to show slip scale on diagram
+        show_eff_scale=True,       # whether to show slip scale on diagram
         show_slip_scale=True,      # whether to show slip scale on diagram
         show_pf_curve=True,        # whether to show power factor curve scale on diagram
-        show_full_circle=False,     # whether to show full circle on diagram
+        show_full_circle=False,    # whether to show full circle on diagram
         show_grid=True             # whether to show grid lines on diagram
     )
 
